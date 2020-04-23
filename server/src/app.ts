ac4dd489587329a4a2533798e8c2b3a7321e65d8
Leon Salsiccia <leon.salsiccia@gmail.com>
2020-04-23 19:38:26 +1000

    2Captcha Auth (#1)
    * first draft
    
    * Finished 2captcha for shows

--- server/src/app.ts
+++ server/src/app.ts
@@ -2,28 +2,12 @@ import express from "express";
 import cors from "cors";
 import { showsRoutes } from "./routes";
 
-import Pup from "./Pup";
-
-let web: Pup;
-(async () => {
-  web = await new Pup().init();
-  await web.load();
-})();
+if (!process.env["apikey"])
+  throw new Error("No 2captcha 'apikey' env variable set.");
 
 const app = express();
-
 app.use(express.json());
 app.use(cors());
-
-app.get("/api/captcha", async (req, res) => {
-  try {
-    const captchaToken = await web.getNewToken();
-    res.json(captchaToken);
-  } catch (err) {
-    console.error("SERVER ERROR:", err.message);
-    res.status(503).json({ error: err.message });
-  }
-});
 
 // Routes
 app.use("/api/shows", showsRoutes);
--- server/src/routes/shows.ts
+++ server/src/routes/shows.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import fetch from "node-fetch";
+import Auther, { IAuthResponse } from "../Auther";
 
 const router = express.Router();
 
@@ -54,35 +55,20 @@ router.get("/seasons/:slug", async (req, res) => {
  * Authenticate for a show.
  * Show slug and Google ReCaptcha token required as query params.
  */
-interface IAuthBody {
-  slug: string;
-  captchaToken: string;
-}
+router.get("/auth/:slug", async (req, res) => {
+  const { slug } = req.params;
+  const auther = new Auther();
 
-interface IAuthResponse {
-  success: boolean;
-  data: {
-    expires: number;
-    accessToken: string;
-  };
-}
+  auther.slug = slug;
+  await auther.sendCaptcha();
+  await auther.waitForResult();
 
-const authUrl = "https://false-promise.lookmovie.ag/api/v1/storage/shows/";
-router.post("/auth", async (req, res) => {
-  const { slug, captchaToken } = req.body as IAuthBody;
-  const queryString = `?slug=${slug}&token=${captchaToken}&sk=null&step=1`;
-
-  const authRes = await fetch(authUrl + queryString);
-
-  if (!authRes.ok)
-    return res
-      .status(authRes.status)
-      .json("Something went wrong: " + authRes.statusText);
-
-  const auth = (await authRes.json()) as IAuthResponse;
-  if (!auth.success) return res.status(503).json("Auth failed.");
-
-  res.json(auth);
+  try {
+    const auth = await auther.auth();
+    res.json(auth);
+  } catch (error) {
+    res.status(401).json("Auth failed.");
+  }
 });
 
 /**
@@ -101,7 +87,7 @@ interface IMasterResponse {
 
 router.post("/episode/:id", async (req, res) => {
   const { auth } = req.body as IQualityBody;
-  const { accessToken, expires } = auth.data;
+  const { accessToken, expires } = auth;
   const { id: episodeId } = req.params;
 
   const masterUrl = `https://dev.lookmovie.ag/manifests/shows/json/${accessToken}/${expires}/${episodeId}/master.m3u8`;
@@ -113,58 +99,6 @@ router.post("/episode/:id", async (req, res) => {
   const qualities = (await masterRes.json()) as IMasterResponse;
 
   res.json(qualities);
-});
-
-/**
- * ########################
- * !!__MAYBE NOT NEEDED__!!
- * #######################
- */
-/**
- * Get the index.m3u8 file for an episode.
- * Episode ID and Auth Access Token and Expiry required from /auth.
- */
-interface IIndexBody {
-  indexUrl: string;
-  auth: IAuthResponse;
-}
-
-router.post("/:id/index", async (req, res) => {
-  const { auth } = req.body as IIndexBody;
-  const { accessToken, expires } = auth.data;
-  const { id: episodeId } = req.params;
-
-  // Create master.m3u8 URL and fetch file
-  // const masterUrl = `https://dev.lookmovie.ag/manifests/shows/json/${auth.data.accessToken}/${auth.data.expires}/${episodeId}/master.m3u8`;
-  // const masterFileRes = await fetch(masterUrl);
-  // if (!masterFileRes.ok)
-  //   return res
-  //     .status(masterFileRes.status)
-  //     .json("Something went wrong: " + masterFileRes.statusText);
-  // const masterFileText = await masterFileRes.text();
-
-  // Create master.m3u8 URL and fetch episode quality links
-  const masterUrl = `https://dev.lookmovie.ag/manifests/shows/json/${accessToken}/${expires}/${episodeId}/master.m3u8`;
-  const masterRes = await fetch(masterUrl);
-  if (!masterRes.ok)
-    return res
-      .status(masterRes.status)
-      .json("Something went wrong: " + masterRes.statusText);
-  const masterData = (await masterRes.json()) as IMasterResponse;
-
-  // Get index.m3u8 URL from master.m3u8 file
-  const bestQualityUrl =
-    masterData["1080"] || masterData["720"] || masterData["480"];
-  if (!bestQualityUrl)
-    return res.status(404).json("Can't find any links for episode.");
-  const indexFileRes = await fetch(bestQualityUrl);
-  if (!indexFileRes.ok)
-    return res
-      .status(indexFileRes.status)
-      .json("Something went wrong: " + indexFileRes.statusText);
-  const indexBuffer = await indexFileRes.buffer();
-
-  res.header("Content-Type", "application/vnd.apple.mpegurl").end(indexBuffer);
 });
 
 export default router;
